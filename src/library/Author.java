ab9fc4b6eec97f8f09bea1ec6bec8bd40a60a236
Crestfall <crestfall2000-real@yahoo.com>
2020-05-04 12:43:58 +0700

    initial commit

---
+++ src/library/Author.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author Crestfall
+ */
+public class Author {
+    
+    
+    
+    String authorName;
+    String authorEmail;
+    char authorGender;
+    
+    public Author(String name, String email, char gender) {
+        authorName = name;
+        authorEmail = email;
+        authorGender = gender;
+    }
+}
