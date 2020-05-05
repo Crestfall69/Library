99d4762f61f190ab968ff876823d84e105fee148
Crestfall <crestfall2000-real@yahoo.com>
2020-05-05 19:24:48 +0700

    actual initial commit

--- src/library/Book.java
+++ src/library/Book.java
@@ -5,16 +5,123 @@
  */
 package library;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Crestfall
  */
 public class Book {
-    
+
     /**
      *
+     * @param name
      */
-    public Book () {
-        
+    public void setBookName(String name) {
+        bookName = name;
+    }
+
+    /**
+     *
+     * @param authors
+     */
+    public void setBookAuthors(Author authors) {
+        bookAuthors.add(authors);
+    }
+
+    /**
+     *
+     * @param price
+     */
+    public void setBookPrice(double price) {
+        bookPrice = price;
+    }
+
+    /**
+     *
+     * @param qty
+     */
+    public void setBookQty(int qty) {
+        bookQty = qty;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getBookName() {
+        return bookName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Author> getBookAuthors() {
+        return bookAuthors;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getBookQty() {
+        return bookQty;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getBookAuthorsNames() {
+        String bookAuthorsNames = "";
+        for (int i = 0; i < getBookAuthors().size(); i++) {
+            if (i == 0) {
+                bookAuthorsNames = bookAuthorsNames + getBookAuthors().get(i);
+            } else {
+                bookAuthorsNames = bookAuthorsNames + ", " + getBookAuthors().get(i);
+            }
+        }
+        return bookAuthorsNames;
+    }
+
+    String bookName;
+    ArrayList<Author> bookAuthors;
+    double bookPrice;
+    int bookQty;
+
+    /**
+     *
+     * @param name
+     * @param authors
+     * @param price
+     */
+    public Book(String name, ArrayList<Author> authors, double price) {
+        bookName = name;
+        bookAuthors = authors;
+        bookPrice = price;
+        bookQty = 0;
+    }
+
+    /**
+     *
+     * @param name
+     * @param authors
+     * @param price
+     * @param qty
+     */
+    public Book(String name, ArrayList<Author> authors, double price, int qty) {
+        bookName = name;
+        bookAuthors = authors;
+        bookPrice = price;
+        bookQty = qty;
     }
 }
